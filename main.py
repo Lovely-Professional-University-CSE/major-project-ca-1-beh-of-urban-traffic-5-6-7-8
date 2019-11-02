e4c619ade576cbc3b95addafd8782140417e4a3e
Pranav Patel <48914365+pranav245@users.noreply.github.com>
2019-11-02 11:34:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,3 +12,16 @@ Chemistry = ctrl.Antecedent(np.arange(0,101,1),'Chemistry')
 Biology = ctrl.Antecedent(np.arange(0,101,1),'Biology')
 
 
+
+
+
+# Adding profession
+
+Engineering = ctrl.Consequent(np.arange(0,1.1,0.1),'Engineering')
+Medicine = ctrl.Consequent(np.arange(0,1.1,0.1),'Medicine')
+Management = ctrl.Consequent(np.arange(0,1.1,0.1),'Management')
+Hospitality = ctrl.Consequent(np.arange(0,1.1,0.1),'Hospitality')
+Chief = ctrl.Consequent(np.arange(0,1.1,0.1),'Chief')
+Teacher = ctrl.Consequent(np.arange(0,1.1,0.1),'Teacher')
+Researcher = ctrl.Consequent(np.arange(0,1.1,0.1),'Researcher')
+Architect = ctrl.Consequent(np.arange(0,1.1,0.1),'Architect')
